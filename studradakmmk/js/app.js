cf63ca9b9135b860716e010103406624dc6867f1
Vadim Kharlaienko <kharlayenko@gmail.com>
2019-11-30 23:39:41 +0200

    123

--- studradakmmk/js/app.js
+++ studradakmmk/js/app.js
@@ -1,22 +1,14 @@
-const hamburger = document.querySelector('.hamburger'),
-   menuItems = document.querySelector('.menu__list'),
-   menuLinks = document.querySelectorAll('.menu__item'),
-   introScroll = document.querySelector('.intro__scroll'),
-   burgerLine = document.querySelectorAll('.line'),
-   btnScroll = document.querySelector('.intro__btn');
+const btnScroll = document.querySelector('a.scroll-down');
 
-hamburger.addEventListener('click', () => {
-   menuItems.classList.toggle('open');
-   hamburger.classList.toggle('hamburger-open');
-   introScroll.classList.toggle('intro__scroll-open');
-   burgerLine.classList.toggle('open-line');
-});
+for (let anchor of anchors) {
+   anchor.addEventListener('click', function(e) {
+      e.preventDefault();
 
-btnScroll.addEventListener('click', function(event) {
-   event.preventDefault();
-   const blockId = btnScroll.getAttribute('href');
-   document.querySelector('' + blockId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      const blockID = anchor.getAttribute('href');
+
+      document.querySelector(blockID).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start'
+      });
    });
-});
+}
