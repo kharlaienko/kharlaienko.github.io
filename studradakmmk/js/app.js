79f0649e494c2ba4479668813be45be06d4239b9
Vadim Kharlaienko <kharlayenko@gmail.com>
2019-12-01 21:56:44 +0200

    123

--- studradakmmk/js/app.js
+++ studradakmmk/js/app.js
@@ -10,6 +10,7 @@ const menu_op_2 = document.getElementById('menu_op_2');
 const menu_3 = document.getElementById('menu_3');
 const menu_op_3 = document.getElementById('menu_op_3');
 const header = document.querySelector('.header');
+const scrollDown = document.querySelector('.scroll-down');
 
 btnScroll.addEventListener('click', function(event) {
    event.preventDefault();
@@ -26,13 +27,20 @@ hamburger.addEventListener('click', () => {
    sliderBtnPrev.classList.toggle('slider__btn_open');
    menuItems.classList.toggle('menu_open');
    header.classList.toggle('header_menu');
+   scrollDown.classList.toggle('scroll-down_open');
 });
 menu_1.addEventListener('click', () => {
    menu_op_1.classList.toggle('submenu_open');
+   menu_op_2.classList.remove('submenu_open');
+   menu_op_3.classList.remove('submenu_open');
 });
 menu_2.addEventListener('click', () => {
    menu_op_2.classList.toggle('submenu_open');
+   menu_op_1.classList.remove('submenu_open');
+   menu_op_3.classList.remove('submenu_open');
 });
 menu_3.addEventListener('click', () => {
    menu_op_3.classList.toggle('submenu_open');
+   menu_op_2.classList.remove('submenu_open');
+   menu_op_1.classList.remove('submenu_open');
 });
