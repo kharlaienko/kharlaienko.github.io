22c5396f15e0d2ed2c3d711004a7ac227c94bb84
kharlaienko <55911912+kharlaienko@users.noreply.github.com>
2021-06-05 22:52:16 +0300

    Add files via upload

---
+++ IrlandiaTour/app.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+   const orderBtn = document.querySelector('.order-brn');
+   const popup = document.querySelector('.popup');
+   const popupForm = document.querySelector('.popup__form');
+   const popupFormThanks = document.querySelector('.popup-form__thanks');
+   const btnContact = document.querySelector('#contacts')
+
+   orderBtn.addEventListener('click', popupOpen);
+   btnContact.addEventListener('click', popupOpen);
+
+   function popupOpen(){
+      orderBtn.removeEventListener('click', popupOpen);
+      btnContact.removeEventListener('click', popupOpen);
+      popup.style.position = 'absolute';
+      popup.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+
+      let animTimeOut = setTimeout(function(){
+         popup.classList.add('popup_open');
+         popupForm.classList.add('popup__form_open')
+         clearTimeout(animTimeOut);
+
+         popup.addEventListener('click', closePopUp);
+         popupForm.addEventListener('click', popupFormClick);
+      });
+   }
+
+   function closePopUp(){
+      popup.style.position = 'unset';
+      popup.style.display = 'none';
+      document.body.style.overflow = 'auto';
+
+      popupFormThanks.classList.remove('popup-form__thanks_open');
+      popup.classList.remove('popup_open');
+      popupFormClose();
+
+      orderBtn.addEventListener('click', popupOpen);
+      btnContact.addEventListener('click', popupOpen);
+      popupForm.removeEventListener('click', popupFormClick);
+   }
+
+   function popupFormClick(event){
+      event.stopPropagation();
+      event.preventDefault();
+
+      if (event.target.type === 'submit'){
+         popupFormThanks.classList.add('popup-form__thanks_open');
+         popupFormClose();
+
+         let waitToThanks = setTimeout(function() {
+            popupForm.reset();
+            closePopUp();
+            clearTimeout(waitToThanks);
+         }, 1000);
+      }
+   }
+
+   function popupFormClose(){
+      popupForm.classList.remove('popup__form_open');
+   }
+});
